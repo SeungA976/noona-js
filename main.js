a9141722cc58701da8ce7f9a5efd2d76bf0a9161
SeungaA <40053398+SeungaA@users.noreply.github.com>
2024-02-13 01:01:04 +0900

    second

--- main.js
+++ main.js
@@ -9,11 +9,20 @@
 //유저가 이미 입력한 숫자를 또 입력하면 또 알려준다. 기회를 깍지 않는다.
 
 let computerNum =0;
+//""안에는 html id 값 그대로 쓰기
 let playButton = document.getElementById("playbutton");
 let userinput = document.getElementById("userinput");
-let resultArea = document.getElementById("result-area")
+let resultArea = document.getElementById("result-area");
+let resetBotton = document.getElementById("reset-button");
+//기회 5번
+let chances = 5
+let gameOver = false
+let chanceArea = document.getElementById("chance-area");
+let history=[];
 
 playButton.addEventListener("click",play);
+resetBotton.addEventListener("click",reset);
+userinput.addEventListener("focus",function(){userinput.value="play"})
 
 function pickRandomNum(){
     computerNum = Math.floor(Math.random()*100)+1;
@@ -22,12 +31,46 @@ function pickRandomNum(){
 
 function play(){
     let userValue = userinput.value;
+
+    //유효성 검사
+    if(userValue<1 || userValue>100){
+        resultArea.textContent="1과 100사이 숫자를 입력해주세요"
+        return;
+    }
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요. "
+        return;
+    }
+    chances -- ;
+    //정적과 동적 변수를 같이 쓸때!!
+    chanceArea.textContent = `남은기회:${chances}번`;
+    console.log("chance",chances);
     if(userValue < computerNum){
         resultArea.textContent = "Up!!"
     }else if(userValue>computerNum){
         resultArea.textContent = "Down!!"
     }else {
         resultArea.textContent = "정답입니다!"
+        gameOver=true
     }
+    history.push(userValue);
+    console.log(history)
+    if(chances<1){
+        gameOver=true;
+    }
+    if (gameOver==true){
+        playButton.disabled = true;
+    }
+    }
+
+
+function reset(){
+    //user input 창이 깨끗하게 정리되고
+    userinput.value = ""
+    //새로운 번호가 생성되고
+    pickRandomNum();
+    resultArea.textContent="결과값이 여기 나옵니다."
 }
+
 pickRandomNum();
+
